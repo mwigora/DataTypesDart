69d02be4a71288339ffd1804d34bd9ebbc1c498b
Horace <joelhorace2@gmail.com>
2024-03-03 21:15:10 +0300

    DataTypesdart

---
+++ DataTypes.dart
@@ -0,0 +1,27 @@
+void main() {
+  int num1 = 20;
+
+  double num2 = 3.14;
+  print(num1 + num2);
+
+  String name = "Joel Horace";
+  print("$name");
+  String socials = "Follow me on instagram @__horacee__";
+  print("$socials");
+
+  List<String> names = ["Joel", "Horace", "Mwigora"];
+  print("Value of name is $names");
+
+  Map gamingcompanies = {"Fifa": "EAsports", "GTA V": "RockstarGames"};
+
+  print(num1);
+  print(num2);
+  print(name);
+  print(names);
+  print(gamingcompanies);
+
+  bool isComing = false;
+  print("Coming status: $isComing");
+
+  ;
+}
